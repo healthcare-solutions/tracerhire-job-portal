04f304a06455f63a358173492cf1f3c8afbd07f9
Mounish Soni <mounishsoni116@gmail.com>
2023-05-01 21:56:37 -0400

    removed CountUp package

--- components/fun-fact-counter/Funfact.jsx
+++ components/fun-fact-counter/Funfact.jsx
@@ -1,40 +1,40 @@
-import { useState } from "react";
-import CountUp from "react-countup";
-import { InView } from "react-intersection-observer";
+// import { useState } from "react";
+// import CountUp from "react-countup";
+// import { InView } from "react-intersection-observer";
 
 const Funfact = () => {
-  const [focus, setFocus] = useState(false);
+  // const [focus, setFocus] = useState(false);
 
-  const counterUpContent = [
-    {
-      id: 1,
-      startCount: "0",
-      endCount: "4",
-      endPointText: "M",
-      meta: "4 million daily active users",
-      animationDelay: "0",
-    },
-    {
-      id: 2,
-      startCount: "0",
-      endCount: "12",
-      endPointText: "k",
-      meta: "Over 12k open job positions",
-      animationDelay: "100",
-    },
-    {
-      id: 3,
-      startCount: "0",
-      endCount: "20",
-      endPointText: "M",
-      meta: "Over 20 million stories shared",
-      animationDelay: "200",
-    },
-  ];
+  // const counterUpContent = [
+  //   {
+  //     id: 1,
+  //     startCount: "0",
+  //     endCount: "4",
+  //     endPointText: "M",
+  //     meta: "4 million daily active users",
+  //     animationDelay: "0",
+  //   },
+  //   {
+  //     id: 2,
+  //     startCount: "0",
+  //     endCount: "12",
+  //     endPointText: "k",
+  //     meta: "Over 12k open job positions",
+  //     animationDelay: "100",
+  //   },
+  //   {
+  //     id: 3,
+  //     startCount: "0",
+  //     endCount: "20",
+  //     endPointText: "M",
+  //     meta: "Over 20 million stories shared",
+  //     animationDelay: "200",
+  //   },
+  // ];
 
   return (
     <>
-      {counterUpContent.map((val) => (
+      {/* {counterUpContent.map((val) => (
         <div
           className="counter-column col-lg-4 col-md-4 col-sm-12 "
           data-aos="fade-up"
@@ -66,7 +66,7 @@ const Funfact = () => {
           </div>
           <h4 className="counter-title">{val.meta}</h4>
         </div>
-      ))}
+      ))} */}
     </>
   );
 };
--- components/fun-fact-counter/Funfact3.jsx
+++ components/fun-fact-counter/Funfact3.jsx
@@ -1,44 +1,44 @@
-import { useState } from "react";
-import CountUp from "react-countup";
-import { InView } from "react-intersection-observer";
+// import { useState } from "react";
+// import CountUp from "react-countup";
+// import { InView } from "react-intersection-observer";
 
 const Funfact3 = () => {
-  const [focus, setFocus] = useState(false);
+  // const [focus, setFocus] = useState(false);
 
-  const counterUpContent = [
-    {
-      id: 1,
-      startCount: "0",
-      endCount: "97216",
-      meta: "Jobs",
-      animationDelay: "700",
-    },
-    {
-      id: 2,
-      startCount: "0",
-      endCount: "4782",
-      meta: "Members",
-      animationDelay: "800",
-    },
-    {
-      id: 3,
-      startCount: "0",
-      endCount: "5322",
-      meta: "Resume",
-      animationDelay: "900",
-    },
-    {
-      id: 4,
-      startCount: "0",
-      endCount: "6329",
-      meta: "Company",
-      animationDelay: "1000",
-    },
-  ];
+  // const counterUpContent = [
+  //   {
+  //     id: 1,
+  //     startCount: "0",
+  //     endCount: "97216",
+  //     meta: "Jobs",
+  //     animationDelay: "700",
+  //   },
+  //   {
+  //     id: 2,
+  //     startCount: "0",
+  //     endCount: "4782",
+  //     meta: "Members",
+  //     animationDelay: "800",
+  //   },
+  //   {
+  //     id: 3,
+  //     startCount: "0",
+  //     endCount: "5322",
+  //     meta: "Resume",
+  //     animationDelay: "900",
+  //   },
+  //   {
+  //     id: 4,
+  //     startCount: "0",
+  //     endCount: "6329",
+  //     meta: "Company",
+  //     animationDelay: "1000",
+  //   },
+  // ];
 
   return (
     <>
-      {counterUpContent.map((val) => (
+      {/* {counterUpContent.map((val) => (
         <div
           className="counter-column col-lg-auto col-md-6 col-sm-6"
           data-aos="fade-up"
@@ -69,7 +69,7 @@ const Funfact3 = () => {
           </div>
           <h4 className="counter-title">{val.meta}</h4>
         </div>
-      ))}
+      ))} */}
     </>
   );
 };
